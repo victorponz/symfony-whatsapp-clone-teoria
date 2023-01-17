f2fabb8427b32fc2bc8de656bb1f7fe754bb4313
Víctor Ponz <victorponz@gmail.com>
2023-01-17 19:11:09 +0100

    Whatsapp clone

---
+++ _site/assets/js/main.js
@@ -0,0 +1,303 @@
+function isAnyPartOfElementInViewport(el) {
+
+    const rect = el.getBoundingClientRect();
+    // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+    
+    // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+    
+    return (vertInView && horInView);
+}
+function addClass(el, className){
+    if (el.classList)
+        el.classList.add(className);
+    else
+        el.className += ' ' + className;
+}
+function removeClass(el, className){
+    if (el.classList)
+        el.classList.remove(className);
+    else
+        el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+
+}
+function hasClass(el, className){
+    if (el.classList)
+        return el.classList.contains(className);
+    else
+        return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+}
+function toggleClass(el, className){
+
+    if (el.classList) {
+        el.classList.toggle(className);
+    } else {
+        var classes = el.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+    
+        if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+        else
+        classes.push(className);
+    
+        el.className = classes.join(' ');
+    }
+  
+}
+function unactive(){
+    allMenuTopics.forEach(el => {
+        removeClass(el, 'active');
+    });
+   
+}
+function isAnyHeadingVisible(){
+    let found = false;
+    allHeaders.forEach(el => {
+        if (isAnyPartOfElementInViewport(el)){
+            found = true;
+         }
+    });
+    return found;
+}
+function activeCurrentTocItem(){
+    allHeaders.forEach(el => {
+        if (isAnyPartOfElementInViewport(el)){
+            unactive();
+            lastHeadingActive = decodeURIComponent(el.getAttribute('id'));
+            lastActive = menuFixed.querySelector("[href='#" + lastHeadingActive + "']");
+            if (lastActive){
+		    addClass(lastActive, 'active');
+		    if (!isAnyPartOfElementInViewport(lastActive)){
+		        lastActive.scrollIntoView();
+		    }
+            }
+	}
+    });
+}
+var menu = document.getElementById("menu");
+var menuFixed = document.getElementById("menuFixed");
+var topFixed = document.getElementById("topFixed");
+var indexFixed = document.getElementById("indexFixed");
+var menuContainer =  document.getElementById("menuContainer");
+var allHeaders =  document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+if (menuFixed)
+	var allMenuTopics = menuFixed.querySelectorAll('a');
+let lastScrollTop = 0;
+let lastActive;
+let lastHeadingActive;
+var backToTop = document.getElementById("back-to-top-link");
+
+window.addEventListener('scroll', function(e) {
+    if (!menuFixed)
+    	return;
+    //Mostrar/ocultar el menú
+    if (!isAnyPartOfElementInViewport(menu)){
+        addClass(menuFixed, "show");
+        addClass(topFixed, "show");
+        //addClass(indexFixed, "show");
+        backToTop.style.display = 'inline';
+    }else{
+        removeClass(menuFixed, "show");
+        removeClass(topFixed, "show");
+        //removeClass(indexFixed, "show");
+        backToTop.style.display = 'none';
+    }
+
+    //Mostrar en el meńu la opción activa
+    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+    var isAnyVisible = isAnyHeadingVisible();
+    
+    activeCurrentTocItem();
+    //Si estamos yendo hacia arriba y no se ve ninguna heading, hemos de activar el anterior a lastActive
+     if ((st <= lastScrollTop) && (!isAnyVisible)) {
+        //Elijo el li anterior al actual para activarlo,
+        let previousHeader;
+        for(var i = 0; i < allHeaders.length; i++){
+            if (lastHeadingActive == allHeaders[i].getAttribute("id")){
+                break;
+            }
+            previousHeader = allHeaders[i];
+        }
+        if (previousHeader){
+            unactive();
+            var tocItem = menuFixed.querySelector("[href='#" + previousHeader.getAttribute("id") + "']");
+            if (tocItem){
+		    addClass(tocItem, 'active');
+		    if (!isAnyPartOfElementInViewport(tocItem)){
+		        tocItem.scrollIntoView();
+		    }
+	   }
+        }
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+   
+});
+if (menuFixed)
+	menuFixed.querySelectorAll('li a').forEach(el => {
+	    el.addEventListener("click", e => {
+		e.preventDefault();
+		let hash = decodeURIComponent(e.currentTarget.href.split("#")[1]);
+		let destination = document.getElementById(hash);
+		destination.scrollIntoView({ behavior: 'smooth' })
+		window.setTimeout(() =>location.hash = hash, 500);
+	    });
+	});
+
+topFixed.querySelector('.toogle').addEventListener("click", e => {
+    e.preventDefault();
+    let el = e.currentTarget;
+    if (hasClass(el, 'expanded')){
+        addClass(menuContainer, 'hide-menu');
+    }else{
+        removeClass(menuContainer, 'hide-menu');
+    }
+    toggleClass(el, 'expanded');
+});
+
+function showMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+//const modalTriggers = document.querySelectorAll('.popup-trigger')
+//const modalCloseTrigger = document.querySelector('.popup-modal__close')
+//const bodyBlackout = document.querySelector('.body-blackout')
+//const iframeIndice = document.querySelector('#iframeIndice');
+
+//Eliminamos la ventana de índice
+// modalTriggers.forEach(trigger => {
+//   trigger.addEventListener('click', () => {
+//     const { popupTrigger } = trigger.dataset
+//     const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
+//     iframeIndice.classList.add('is--visible')
+//     popupModal.classList.add('is--visible')
+//     bodyBlackout.classList.add('is-blacked-out')
+    
+//     popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+//        popupModal.classList.remove('is--visible')
+//        bodyBlackout.classList.remove('is-blacked-out')
+//        iframeIndice.classList.remove('is--visible')
+//     })
+    
+//     bodyBlackout.addEventListener('click', () => {
+//       // TODO: Turn into a function to close modal
+//       popupModal.classList.remove('is--visible')
+//       bodyBlackout.classList.remove('is-blacked-out')
+//     })
+//   })
+// })
+
+document.querySelectorAll('div.highlighter-rouge').forEach(el => {
+    const newNode = document.createElement("div");
+    newNode.className= 'copy-code hidden';
+    newNode.innerHTML = 'Copy'; 
+    newNode.addEventListener("click", e => {
+        e.preventDefault();
+        const code = el.querySelector('td.rouge-code');
+        navigator.clipboard.writeText(code.innerText).then(function() {
+            newNode.innerText = 'Copied';
+            setTimeout(function(){newNode.innerText = 'Copy'}, 1000);
+          }, function(err) {
+            console.error('Async: Could not copy text: ', err);
+          });
+    });
+    el.insertBefore(newNode, el.firstChild);
+    el.addEventListener("mouseover", e => {
+        e.preventDefault();
+        if (hasClass(newNode, 'hidden'))
+            toggleClass(newNode, 'hidden');
+    });
+    el.addEventListener("mouseout", e => {
+        e.preventDefault();
+        addClass(newNode, 'hidden');
+    });
+});
+
+
+document.querySelectorAll('blockquote p.task').forEach(el => {
+   	addClass(el.parentElement, "task");
+});
+
+document.querySelectorAll('blockquote p.reto').forEach(el => {
+   	addClass(el.parentElement, "reto");
+});
+
+document.querySelectorAll('blockquote p.hint').forEach(el => {
+   	addClass(el.parentElement, "hint");
+});
+
+document.querySelectorAll('blockquote p.info').forEach(el => {
+   	addClass(el.parentElement, "info");
+});
+
+document.querySelectorAll('blockquote p.warning').forEach(el => {
+   	addClass(el.parentElement, "warning");
+});
+
+document.querySelectorAll('blockquote p.alert').forEach(el => {
+   	addClass(el.parentElement, "alert");
+});
+
+document.querySelectorAll('blockquote p.toogle').forEach(el => {
+   	
+   	var firstParagraf = el.parentNode.querySelector("p");
+   	//<summary>Click to toggle contents of `code`</summary>
+	var that = el.parentElement;
+
+	var p = document.createElement('details');
+	addClass(p, "toogle");
+
+	// move all elements in the other container.
+	while(that.firstChild) {
+		p.appendChild(that.firstChild);
+	}
+	that.parentNode.replaceChild(p,that);
+	
+	const newNode = document.createElement("summary");
+	newNode.innerHTML = firstParagraf.innerText;
+	if (firstParagraf.innerText != "")
+		newNode.innerHTML = firstParagraf.innerText;
+	else{
+		newNode.innerHTML = firstParagraf.innerHTML;
+	}
+	el.parentElement.insertBefore(newNode, el.parentElement.firstChild);
+	el.parentElement.removeChild(firstParagraf);
+});
+
+//Sequence
+document.querySelectorAll('code.language-sequence').forEach((el , i) => {
+   	addClass(el.parentElement, "language-sequence");
+   	const newNode = document.createElement("div");
+    	newNode.id= 'diagram-' + i;
+        el.parentElement.insertBefore(newNode, el.parentElement.firstChild);
+        var diagram = Diagram.parse(el.innerText);
+        diagram.drawSVG(newNode.id, {theme: 'simple'});
+        addClass(el, "hidden");
+});
+
+//Mermaid
+document.querySelectorAll('code.language-mermaid').forEach((el , i) => {
+   	addClass(el, "mermaid");
+   	const newNode = document.createElement("div");
+    	newNode.className= 'mermaid-container';
+    	el.parentElement.insertBefore(newNode, el.parentElement.firstChild);
+    	newNode.appendChild(el);
+});
+
+//Flowchart
+document.querySelectorAll('code.language-flow').forEach((el , i) => {
+   	addClass(el.parentElement, "language-flow");
+   	const newNode = document.createElement("div");
+    	newNode.id= 'flow-diagram-' + i;
+        el.parentElement.insertBefore(newNode, el.parentElement.firstChild);
+        var diagram = flowchart.parse(el.innerText);
+        diagram.drawSVG(newNode.id);
+        addClass(el, "hidden");
+});
+
---
+++ index.md
@@ -0,0 +1,112 @@
+---
+typora-copy-images-to: ../../assets/img/primeros-pasos/
+typora-root-url: ../../
+title: Symfony Whatsapp clone
+layout: post
+categories: parte1
+conToc: true
+---
+En esta parte del módulo vamos a implementar un clon de la aplicación Whatsapp.
+
+Constará de dos partes diferenciadas:
+
+1. El cliente, que es tarea vuestra
+2. El servidor que ya está implementado y puedes descargarlo [aquí](https://github.com/victorponz/whatsapp-server)
+
+### Funcionamiento del cliente
+
+#### Mensajes
+
+* Cuando se activa uno de los chats, todos los mensajes de los participantes se cargan asíncronamente mediante una petición ajax a `localhost:8080/messages/from/{toUserId}`. El método que responde a dicha ruta debe devolver en formato `json`  todos los mensajes enviados o recibidos por `toUserId` y el usuario logeado en el sistema
+* Cuando se envía un mensaje, se debe hacer a la ruta `/post/touser/{toUserId}`. Como todavía no hemos conectado el servidor, para ver el nuevo mensaje hemos de recargar el chat o la página.
+* Os dejo un [esqueleto](https://github.com/victorponz/whatsapp-clone-skeleton) de la aplicación cliente, que contiene todo lo necesario para poder empezar, incluida una plantilla, y las entidades `User` y `Messsge` junto con sus repositorios.
+  Una vez descargado el esqueleto, realiza una migración para que se creen las tablas.
+
+#### Contactos
+
+Los contactos también se recuperan mediante una llamada asíncrona a `/contacts/{userId}`. En esta primera versión vamos a mostrar **todos** los usuarios registrados.
+
+### Funcionamiento del servidor
+
+Como conocéis, Whatsapp actualiza automáticamente los mensajes cuando recibe uno nuevo. Pero, ¿cómo implementar esta funcionalidad en una aplicación web?
+
+En una aplicación web, el inicio de la comunicación entre un cliente y un servidor en el protocolo `HTTP` **siempre** lo realiza el cliente. Una vez finalizada esta comunicación se **cierra** la conexión.
+
+Así, podemos montar una función en javascript que mediante uso de `setTimeOut` llamase a un api que recuperase los nuevos mensajes. Pero claro, en cuánto tiempo fijo el `timeout`: 1/2 segundo, 1 segundo.... Seguramente si llamo muy deprisa, la mayoría de veces no habrá mensajes nuevos mientras que si lo hago más lento los recuperaré en paquetes.
+
+Es mejor usar una tecnología llamada [websockets](https://developer.mozilla.org/es/docs/Web/API/WebSockets_API). En este caso, se inicia un canal de comunicación bidireccional que se mantiene **siempre abierto**. Ahora el servidor y el cliente están continuamente *escuchando* la llegada de nuevos mensajes.
+
+Para lanzar el servidor desde la raíz del proyecto, usa este comando:
+
+```
+ php -q bin/server port:9000
+```
+
+Esto iniciará el servidor y empezará a escuchar la conexión de nuevos usuarios.
+
+Conectar el cliente es muy sencillo:
+
+```javascript
+$(document).ready(function(){ç
+	//Open a WebSocket connection.
+	websocket = new WebSocket("ws://localhost:9000/");
+	
+    //Connected to server
+	websocket.onopen = function(ev) {
+		console.log('Connected to server ');
+	}
+    
+    //Connection close
+	websocket.onclose = function(ev) { 
+    	console.log('Disconnected');
+    };
+    websocket.onmessage = function(evt) { 
+        var response 		= JSON.parse(evt.data); //PHP sends Json data
+        //hacer lo que corresponda con response
+    };
+     
+    //Error
+	websocket.onerror = function(ev) { 
+    	console.log('Error '+ev.data);
+    };
+    
+});
+```
+
+Ahora, cada vez que el servidor nos envíe un mensaje llegará al evento `websocket.onmessage`
+
+El servidor envía dos tipos de mensajes que debéis tratar:
+
+* Cuando se conecta un nuevo usuario, envía el siguiente mensaje:
+
+  ```json
+  {
+    "type": "usermsg",
+    "id": 12,
+    "userName": "sonia",
+    "info": "soy Sonia",
+    "image": "719SdJJgEoL-AC-SX425-63c6deb1056ed.jpg"
+  }
+  ```
+
+* Cuando llega un mensaje nuevo hace un `broadcasting` del mismo y envía el siguiente `json`
+
+  ```json
+  {
+    "type": "chatmsg",
+    "toUserId": 1,
+    "fromUserId": 2,
+    "text": "hola",
+    "timestamp": {
+      "date": "2023-01-17 10:42:44.000000",
+      "timezone_type": 3,
+      "timezone": "Europe/Berlin"
+    },
+    "fromUserName": "yo"
+  }
+  ```
+
+
+  > -warning-En una aplicación real usaríamos otras aproximaciones, como colas de mensajes, [socket.io](https://socket.io/), etc
+
+  
